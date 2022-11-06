cbd9b2022240695332c49a3ceb2665e4cebc4cb7
IvBlack <ivbl21@ya.ru>
2022-11-06 18:25:16 +0300

    Util.java rewrited

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,16 +1,36 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
-import java.util.Properties;
-
-import static jm.task.core.jdbc.util.Util.link;
 
 public class Main {
     public static void main(String[] args) {
+        Util util = new Util();
+        util.link();
+
+        /*
+        String query = "SELECT * from users";
+        try {
+            User user = new User();
+            Statement st = util.getConnection().createStatement();
+            //отдаем в statement запрос к БД
+            ResultSet rs = st.executeQuery(query);
+
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                System.out.println(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+         */
+
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,28 +8,36 @@ import java.util.Properties;
 
 public class Util {
 
-    //создадим  объект java.sql.Connection для коннекта с БД
-    public static Connection link() {
+    /*
+    //попытка использовать вынесенный файл настроек утонула в Exceptions
+    Properties data = new Properties();
+
+    String url = data.getProperty("url");
+    String username = data.getProperty("login");
+    String password = data.getProperty("pass");
+
+    try (InputStream in = Files.newInputStream(Paths.get("db.properties"))) {
+            data.load(in);
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
+    */
+
+    private final static String login = "root";
+    private final static String pass = "yourpasswd";
+    private final static String url = "jdbc:mysql://localhost/pp1_4?serverTimezone=Europe/Moscow&useSSl=false";
+
+    public Connection link() {
+        //инициализируем подключение
         Connection con = null;
 
-       //данные подключения вынесены в db.properties
-        Properties data = new Properties();
-
-        try (InputStream in = Files.newInputStream(Paths.get("db.properties"))) {
-            data.load(in);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        String url = data.getProperty("url");
-        String username = data.getProperty("login");
-        String password = data.getProperty("pass");
-
-        //коннект к БД
         try {
-            con = DriverManager.getConnection("url", "login", "pass");
+            con = DriverManager.getConnection(url, login, pass);
+            System.out.println("Connection done.");
             return con;
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Connection failed.");
         }
         return con;
     }
